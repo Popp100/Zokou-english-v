6660e4d6d763c746d73868184f24c0f18e4dc728
Spellblade Sorcerer <121673607+Popp100@users.noreply.github.com>
2024-07-10 22:50:01 -0400

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,1 +1,85 @@
-function a33_0x2462(){const _0x4a0c5a=['util','categorie','sendMessage','38rItIRz','/../framework/mesfonctions','\x20👋\x0a\x0a*List\x20of\x20commands\x20:*\x0a◇\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20◇\x0a','freemem','Je\x20suis\x20*Zokou-MD*,\x20développé\x20par\x20Djalega++','map','\x0a│\x20\x20\x20*Owner*\x20:\x20','4060ntqEPB','totalmem','\x0a◇\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20◇\x0a*»»—————\x20★\x20—————««*\x0a\x22To\x20use\x20a\x20command,\x20insert\x20','push','╭────❏\x20','🥵🥵\x20Menu\x20erreur\x20','9Wuieqt','DD/MM/YYYY','5023998UDIRnr','nomCom','\x0a│\x20\x20\x20*Date*\x20:\x20','10831680goxAOM','✧────◆\x0a│\x20\x20\x20*Préfix*\x20:\x20','yes','4415Rvpzyl','1825192XBwDaB','log','public','PREFIXE','\x0a╭────✧','\x0a│\x20\x20\x20*Mode*\x20:\x20','/../set','\x0a│\x20\x20\x20*Mémoire*\x20:\x20','BOT','HH:mm:ss','Etc/GMT','fs-extra','format','1195791mYlRGP','MODE','private','\x0a│\x20\x20\x20*Développer*\x20:\x20Djalega++\x20\x0a│\x20\x20&\x20M๏𝓷keℽ\x20D\x20Lบffy\x0a╰─────✧WA-BOT✧─────◆\x20\x0a\x0a','\x20followed\x20by\x20the\x20command_name.\x22\x0a\x20\x0a\x20\x20\x20\x20Powered\x20by\x20Zokou-MD\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*»»—————\x20★\x20—————««*\x0a','moment-timezone','35914VaqSno','\x0a│\x20\x20\x20*Hour*\x20:\x20','/../framework//zokou','length','5077752XjzBLE'];a33_0x2462=function(){return _0x4a0c5a;};return a33_0x2462();}const a33_0x41656b=a33_0xefce;(function(_0x1ed5dc,_0x466d4a){const _0xef335c=a33_0xefce,_0x4b57bc=_0x1ed5dc();while(!![]){try{const _0x40c08e=-parseInt(_0xef335c(0xf1))/0x1*(-parseInt(_0xef335c(0xe9))/0x2)+parseInt(_0xef335c(0xe3))/0x3+-parseInt(_0xef335c(0xf8))/0x4*(-parseInt(_0xef335c(0xd5))/0x5)+-parseInt(_0xef335c(0xed))/0x6+parseInt(_0xef335c(0xcf))/0x7+parseInt(_0xef335c(0xd6))/0x8*(-parseInt(_0xef335c(0xfe))/0x9)+-parseInt(_0xef335c(0xd2))/0xa;if(_0x40c08e===_0x466d4a)break;else _0x4b57bc['push'](_0x4b57bc['shift']());}catch(_0x207e11){_0x4b57bc['push'](_0x4b57bc['shift']());}}}(a33_0x2462,0x832e1));const util=require(a33_0x41656b(0xee)),fs=require(a33_0x41656b(0xe1)),{zokou}=require(__dirname+'/../framework/zokou'),{format}=require(__dirname+a33_0x41656b(0xf2)),os=require('os'),moment=require(a33_0x41656b(0xe8)),s=require(__dirname+a33_0x41656b(0xdc));function a33_0xefce(_0xbb3451,_0x3daee9){const _0x2462f1=a33_0x2462();return a33_0xefce=function(_0xefcefa,_0x1545f5){_0xefcefa=_0xefcefa-0xcf;let _0x5ad12c=_0x2462f1[_0xefcefa];return _0x5ad12c;},a33_0xefce(_0xbb3451,_0x3daee9);}zokou({'nomCom':'menu','categorie':'General'},async(_0x61e41a,_0x4f8896,_0x1971cc)=>{const _0x3a8f5a=a33_0x41656b;let {ms:_0x3616de,repondre:_0x310440,prefixe:_0x5c0bf4,nomAuteurMessage:_0x370c2e,mybotpic:_0x224398}=_0x1971cc,{cm:_0x5d6de6}=require(__dirname+_0x3a8f5a(0xeb));var _0x34289f={},_0xd70f6f=_0x3a8f5a(0xd8);s[_0x3a8f5a(0xe4)]['toLocaleLowerCase']()!=_0x3a8f5a(0xd4)&&(_0xd70f6f=_0x3a8f5a(0xe5));_0x5d6de6[_0x3a8f5a(0xf6)](async(_0xfa52c8,_0x1b9221)=>{const _0xc27969=_0x3a8f5a;if(!_0x34289f[_0xfa52c8[_0xc27969(0xef)]])_0x34289f[_0xfa52c8['categorie']]=[];_0x34289f[_0xfa52c8[_0xc27969(0xef)]][_0xc27969(0xfb)](_0xfa52c8[_0xc27969(0xd0)]);}),moment['tz']['setDefault'](_0x3a8f5a(0xe0));const _0x26a57a=moment()['format'](_0x3a8f5a(0xdf)),_0x39f5be=moment()[_0x3a8f5a(0xe2)](_0x3a8f5a(0xff));let _0x104f7b=_0x3a8f5a(0xda)+s[_0x3a8f5a(0xde)]+_0x3a8f5a(0xd3)+s[_0x3a8f5a(0xd9)]+_0x3a8f5a(0xf7)+s['OWNER_NAME']+_0x3a8f5a(0xdb)+_0xd70f6f+'\x0a│\x20\x20\x20*Commands*\x20:\x20'+_0x5d6de6[_0x3a8f5a(0xec)]+_0x3a8f5a(0xd1)+_0x39f5be+_0x3a8f5a(0xea)+_0x26a57a+_0x3a8f5a(0xdd)+format(os[_0x3a8f5a(0xf9)]()-os[_0x3a8f5a(0xf4)]())+'/'+format(os[_0x3a8f5a(0xf9)]())+'\x0a│\x20\x20\x20*Plateforme*\x20:\x20'+os['platform']()+_0x3a8f5a(0xe6),_0x1f3a4e='\x0a👋\x20Hello\x20'+_0x370c2e+_0x3a8f5a(0xf3);for(const _0x235db9 in _0x34289f){_0x1f3a4e+=_0x3a8f5a(0xfc)+_0x235db9+'\x20❏';for(const _0x24b838 of _0x34289f[_0x235db9]){_0x1f3a4e+='\x0a│\x20✿\x20'+_0x24b838;}_0x1f3a4e+='\x0a╰═════════════⊷\x20\x0a';}_0x1f3a4e+=_0x3a8f5a(0xfa)+_0x5c0bf4+_0x3a8f5a(0xe7);var _0x56c1bb=_0x224398();if(_0x56c1bb['match'](/\.(mp4|gif)$/i))try{_0x4f8896[_0x3a8f5a(0xf0)](_0x61e41a,{'video':{'url':_0x56c1bb},'caption':_0x104f7b+_0x1f3a4e,'footer':_0x3a8f5a(0xf5),'gifPlayback':!![]},{'quoted':_0x3616de});}catch(_0x35c135){console['log'](_0x3a8f5a(0xfd)+_0x35c135),_0x310440(_0x3a8f5a(0xfd)+_0x35c135);}else{if(_0x56c1bb['match'](/\.(jpeg|png|jpg)$/i))try{_0x4f8896[_0x3a8f5a(0xf0)](_0x61e41a,{'image':{'url':_0x56c1bb},'caption':_0x104f7b+_0x1f3a4e,'footer':'Je\x20suis\x20*Zokou-MD*,\x20développé\x20par\x20Djalega++'},{'quoted':_0x3616de});}catch(_0xed152f){console[_0x3a8f5a(0xd7)]('🥵🥵\x20Menu\x20erreur\x20'+_0xed152f),_0x310440(_0x3a8f5a(0xfd)+_0xed152f);}else _0x310440(_0x104f7b+_0x1f3a4e);}});
+const {
+  zokou
+} = require(__dirname + '/../framework/zokou');
+const {
+  format
+} = require(__dirname + "/../framework/mesfonctions");
+const os = require('os');
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+zokou({
+  'nomCom': 'menu',
+  'categorie': 'General'
+}, async (_0x61e41a, _0x4f8896, _0x1971cc) => {
+  let {
+    ms: _0x3616de,
+    repondre: _0x310440,
+    prefixe: _0x5c0bf4,
+    nomAuteurMessage: _0x370c2e,
+    mybotpic: _0x224398
+  } = _0x1971cc;
+  let {
+    cm: _0x5d6de6
+  } = require(__dirname + "/../framework//zokou");
+  var _0x34289f = {};
+  var _0xd70f6f = "public";
+  if (s.MODE.toLocaleLowerCase() != "yes") {
+    _0xd70f6f = "private";
+  }
+  _0x5d6de6.map(async (_0xfa52c8, _0x1b9221) => {
+    if (!_0x34289f[_0xfa52c8.categorie]) {
+      _0x34289f[_0xfa52c8.categorie] = [];
+    }
+    _0x34289f[_0xfa52c8.categorie].push(_0xfa52c8.nomCom);
+  });
+  moment.tz.setDefault("Etc/GMT");
+  const _0x26a57a = moment().format("HH:mm:ss");
+  const _0x39f5be = moment().format("DD/MM/YYYY");
+  let _0x104f7b = "\n╭────✧" + s.BOT + "✧────◆\n│   *Préfix* : " + s.PREFIXE + "\n│   *Owner* : " + s.OWNER_NAME + "\n│   *Mode* : " + _0xd70f6f + "\n│   *Commands* : " + _0x5d6de6.length + "\n│   *Date* : " + _0x39f5be + "\n│   *Hour* : " + _0x26a57a + "\n│   *Mémoire* : " + format(os.totalmem() - os.freemem()) + '/' + format(os.totalmem()) + "\n│   *Plateforme* : " + os.platform() + "\n│   *Développer* : Zenitsu-MD🗡️⚡💫\n╰─────✧WA-BOT✧─────◆ \n\n";
+  let _0x1f3a4e = "\n👋 Hello " + _0x370c2e + " 👋\n\n*List of commands :*\n◇                             ◇\n";
+  for (const _0x235db9 in _0x34289f) {
+    _0x1f3a4e += "╭────❏ " + _0x235db9 + " ❏";
+    for (const _0x24b838 of _0x34289f[_0x235db9]) {
+      _0x1f3a4e += "\n│ ✿ " + _0x24b838;
+    }
+    _0x1f3a4e += "\n╰═════════════⊷ \n";
+  }
+  _0x1f3a4e += "\n◇            ◇\n*»»————— ★ —————««*\n\"To use a command, insert " + _0x5c0bf4 + " followed by the command_name.\"\n \n    Powered by Zenitsu🗡️⚡💫\n                                                \n*»»————— ★ —————««*\n";
+  var _0x56c1bb = _0x224398();
+  if (_0x56c1bb.match(/\.(mp4|gif)$/i)) {
+    try {
+      _0x4f8896.sendMessage(_0x61e41a, {
+        'video': {
+          'url': _0x56c1bb
+        },
+        'caption': _0x104f7b + _0x1f3a4e,
+        'footer': "Je suis *Zenitsu-MD🗡️⚡💫*, développé par Zenitsu🗡️⚡💫",
+        'gifPlayback': true
+      }, {
+        'quoted': _0x3616de
+      });
+    } catch (_0x35c135) {
+      console.log("🥵🥵 Menu erreur " + _0x35c135);
+      _0x310440("🥵🥵 Menu erreur " + _0x35c135);
+    }
+  } else {
+    if (_0x56c1bb.match(/\.(jpeg|png|jpg)$/i)) {
+      try {
+        _0x4f8896.sendMessage(_0x61e41a, {
+          'image': {
+            'url': _0x56c1bb
+          },
+          'caption': _0x104f7b + _0x1f3a4e,
+          'footer': "Je suis *Zenitsu-MD🗡️⚡💫*, développé par Zenitsu🗡️⚡💫"
+        }, {
+          'quoted': _0x3616de
+        });
+      } catch (_0xed152f) {
+        console.log("🥵🥵 Menu erreur " + _0xed152f);
+        _0x310440("🥵🥵 Menu erreur " + _0xed152f);
+      }
+    } else {
+      _0x310440(_0x104f7b + _0x1f3a4e);
+    }
+  }
+});
